6971c28e9487710efc3ea30943697c715cb2a8a5
moatefdev <m.atef317@gmail.com>
2021-07-02 14:33:55 +0200

    first commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+console.log("Hello World");
 
+// app.get("/", function(req, res){
+//   res.send("Hello Express");
+// });
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
